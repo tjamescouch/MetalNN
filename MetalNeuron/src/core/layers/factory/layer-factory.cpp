867bf0f5d8035c3ee71713c0839a6c47ff3c5764
James Couch <timothy.james.couch@gmail.com>
2025-03-18 06:09:21 -0600

    factory updates

--- MetalNeuron/src/core/layers/factory/layer-factory.cpp
+++ MetalNeuron/src/core/layers/factory/layer-factory.cpp
@@ -33,12 +33,27 @@ Layer* LayerFactory::createLayer(LayerConfig& layerConfig,
     auto batchSize = config->training.batch_size;
 
     std::cout << "Getting common layer parameters..." << std::endl;
-    int inputSize = layerConfig.params.at("input_size").get_value<int>();
-    int outputSize = layerConfig.params.at("output_size").get_value<int>();
-    
-    int sequence_length = layerConfig.params["sequence_length"].get_value_or<int>(1);
-    sequence_length = sequence_length > 0 ? sequence_length : 1;
-    
+    int inputSize = 0;
+    int outputSize = 0;
+    int sequenceLength = 1;  // default for non-sequence layers
+
+    // Explicitly handle shapes for sequence-aware layers
+    if (layerConfig.params.contains("input_shape")) {
+        int inputShape[2] = {};
+        layerConfig.params["input_shape"].get_value_inplace(inputShape);
+        sequenceLength = inputShape[0];
+        inputSize = inputShape[1];
+    } else {
+        inputSize = layerConfig.params.at("input_size").get_value<int>();
+    }
+
+    if (layerConfig.params.contains("output_shape")) {
+        int outputShape[2] = {};
+        layerConfig.params["output_shape"].get_value_inplace(outputShape);
+        outputSize = outputShape[1];
+    } else {
+        outputSize = layerConfig.params.at("output_size").get_value<int>();
+    }
 
     // Provide a default sequential numeric ID if name not explicitly provided
     std::string layerName = layerConfig.params["name"].get_value_or<std::string>(
@@ -70,14 +85,14 @@ Layer* LayerFactory::createLayer(LayerConfig& layerConfig,
         
         auto initializer = layerConfig.params["initializer"].get_value_or<std::string>("xavier");
         
-        layer = (new SelfAttentionLayer(inputSize, outputSize, sequence_length, batchSize))->setInitializer(initializer);
+        layer = (new SelfAttentionLayer(inputSize, outputSize, sequenceLength, batchSize))->setInitializer(initializer);
         
     } else if (layerConfig.type == "MultiHeadAttention") {
         std::cout << "Creating multi-head attention layer..." << std::endl;
         int num_heads = layerConfig.params.at("num_heads").get_value_or<int>(2);
         auto initializer = layerConfig.params["initializer"].get_value_or<std::string>("xavier");
         
-        layer = (new MultiHeadAttentionLayer(inputSize, outputSize, sequence_length, batchSize, num_heads))->setInitializer(initializer);
+        layer = (new MultiHeadAttentionLayer(inputSize, outputSize, sequenceLength, batchSize, num_heads))->setInitializer(initializer);
         
     } else if (layerConfig.type == "BatchNormalization") {
         std::cout << "Creating batch normalization layer..." << std::endl;
@@ -89,11 +104,11 @@ Layer* LayerFactory::createLayer(LayerConfig& layerConfig,
         std::cout << "Creating layer normalization layer..." << std::endl;
         float epsilon = layerConfig.params["epsilon"].get_value_or<float>(1e-5f);
         epsilon = epsilon > 0 ? epsilon : 1e-5f;
-        layer = new LayerNormalizationLayer(inputSize, batchSize, sequence_length, learningRate, epsilon);
+        layer = new LayerNormalizationLayer(inputSize, sequenceLength, batchSize, learningRate, epsilon);
         
     } else if (layerConfig.type == "ResidualConnection") {
         auto from = layerConfig.params.at("from_layer").get_value<std::string>();
-        std::cout << "Creating residual connectionn layer from " << from << "..." << std::endl;
+        std::cout << "Creating residual connection layer from " << from << "..." << std::endl;
         layer = (new ResidualConnectionLayer(inputSize, batchSize))
                     ->setResidualInput(layerMap_[from]->getOutputBufferAt(BufferType::Output));
         
